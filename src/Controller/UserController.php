e7e19ddc583f53b2f60daeab01ff2a0a5288ccfd
Kevin Lee De Gouveia <kdg@redwoodtech.com>
2020-07-30 13:36:00 +0100

    Pages 2-3 Done:

---
+++ src/Controller/UserController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserController extends AbstractController
+{
+    private $passwordEncoder;
+
+    public function _construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @Route("/user/new", name="contactbook_new")
+     */
+    public function new()
+    {
+        return $this->render('user/new.html.twig', []);
+    }
+
+    /**
+     * @Route("/user/{id}", name="contactbook")
+     */
+    public function show()
+    {
+        $user = $this->getUser();
+        return $this->render('user/index.html.twig', [
+            'user' => $user,
+            'contacts' => $user->getContact()
+        ]);
+    }
+
+    /**
+     * @Route("/user/{id}/contact", name="contact_create", methods={"POST"})
+     */
+    public function create(Request $request)
+    {
+        $password = $request->request->get('password');
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $request->request->get('email')]);
+        if ($user) {
+            $this->addFlash('notice', 'that email already exists');
+            return $this->redirectToRoute('contactbook_new');
+        } else {
+            $user = new User();
+            $user->setEmail($request->request->get('email'));
+            $user->setPassword($request->passwordEncoder->encodePassword($user, $password));
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_login');
+        }
+    }
+
+    /**
+     * @Route("/user/{id}/contact", name="contact_create", methods={"POST"})
+     */
+    public function create_contact(Request $request)
+    {
+        $user = $this->getUser();
+        $entityManager = $this->getDoctrine()->getManager();
+        $contact = new Contact();
+        $contact->setName($request->request->get('name'));
+        $contact->setTel($request->request->get('tel'));
+        $contact->setUser($user);
+        $entityManager->persist($contact);
+        $entityManager->flush();
+        return $this->redirectToRoute('contactbook', ['id' => $user->getId()]);
+    }
+}
